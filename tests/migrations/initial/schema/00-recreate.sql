4a57fd33fc43a38e858606b976bb1809531bd224
TodayNightt <sd.teoh.tsd@gmail.com>
2025-08-18 20:21:32 +0800

    fix : added schema for `schema` db

---
+++ tests/migrations/initial/schema/00-recreate.sql
@@ -0,0 +1,4 @@
+PRAGMA foreign_keys = OFF;
+DROP TABLE IF EXISTS schema;
+
+PRAGMA foreign_keys = ON;
---
+++ tests/migrations/initial/schema/01-initial.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS schema
+(
+    id     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+    name   TEXT                              NOT NULL,
+    fields TEXT                              NOT NULL 
+);
+
+
+
